fc7fc62fe552e61dec0e9cd69e60d9d6b997f902
Sean Merkel <16smerkel@gmail.com>
2024-11-07 00:56:52 -0500

    Update with latesdmddkdkges

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { SliceZone } from "@prismicio/react";
@@ -41,47 +40,3 @@ export async function generateStaticParams() {
     return { uid: page.uid };
   });
 }
-=======
-import { Metadata } from "next";
-import { notFound } from "next/navigation";
-import { SliceZone } from "@prismicio/react";
-
-import { createClient } from "@/prismicio";
-import { components } from "@/slices";
-
-type Params = { uid: string };
-
-export default async function Page({ params }: { params: Params }) {
-  const client = createClient();
-  const page = await client
-    .getByUID("page", params.uid)
-    .catch(() => notFound());
-
-  return <SliceZone slices={page.data.slices} components={components} />;
-}
-
-export async function generateMetadata({
-  params,
-}: {
-  params: Params;
-}): Promise<Metadata> {
-  const client = createClient();
-  const page = await client
-    .getByUID("page", params.uid)
-    .catch(() => notFound());
-
-  return {
-    title: page.data.meta_title,
-    description: page.data.meta_description,
-  };
-}
-
-export async function generateStaticParams() {
-  const client = createClient();
-  const pages = await client.getAllByType("page");
-
-  return pages.map((page) => {
-    return { uid: page.uid };
-  });
-}
->>>>>>> 513fb1e55267ae49c2a41f1693727e2a9f24668f
